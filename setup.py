54f25dd5bbc12b19dee4ad3b8dd47df9be2c75ff
clara seijo <clasei@proton.me>
2026-01-17 12:47:59 +0100

    fix: configure setup.py to find packages in src/ directory

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/clasei/sinner",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
