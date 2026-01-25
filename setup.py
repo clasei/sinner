7f50339e271fa11020eb6ca6cb9734c37ec52ada
clara seijo <clasei@proton.me>
2026-01-25 10:24:30 +0100

    chore(release) -> bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="sinner",
-    version="0.1.0",
+    version="0.2.0",
     author="clasei",
     description="Local-first CLI agent that turns messy intent into clean output",
     long_description=long_description,
